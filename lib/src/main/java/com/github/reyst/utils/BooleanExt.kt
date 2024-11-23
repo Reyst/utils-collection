64d99c40b40ab5656dc1477639a9e117772bb3e8
Pavlo Oleksenko <oleksenko.pm@gmail.com>
2024-11-23 21:40:11 +0200

    add Boolean defaults for nullable cases

---
+++ lib/src/main/java/com/github/reyst/utils/BooleanExt.kt
@@ -0,0 +1,9 @@
+@file:Suppress("unused")
+
+package com.github.reyst.utils
+
+fun Boolean?.orTrue() = orDefault(true)
+
+fun Boolean?.orFalse() = orDefault(false)
+
+fun Boolean?.orDefault(default: Boolean) = this ?: default
